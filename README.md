0bcf14d2895cb750e929ccb82cb59c9f79ada613
Changkun Ou <hi@changkun.us>
2018-07-01 00:07:40 +0200

    fix: typos

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 goschduler is a task scheduler with data persistence for go.
 
+## Features
+
+- Schedule a task in a specific time
+- Boot (existing) task immediately
+
 ## Usage
 
 see [example](./example/main.go).
--- example/main.go
+++ example/main.go
@@ -15,45 +15,47 @@ type CustomTask struct {
 	Information string    `json:"info"`
 }
 
-// Identifier implement the following methods
+// Identifier should returns a unique string for the task, usually can return an UUID
 func (c CustomTask) Identifier() string {
 	return c.ID
 }
 
-// GetExecuteTime returns the executing time of the task
+// GetExecuteTime should returns the excute time of the task
 func (c CustomTask) GetExecuteTime() time.Time {
 	return c.End
 }
 
-// SetExecuteTime sets the executing time of the task
+// SetExecuteTime can set the execution time of the task
 func (c *CustomTask) SetExecuteTime(t time.Time) time.Time {
 	c.End = t
 	return c.End
 }
 
-// Execute the task
+// Execute defines the actual running task
 func (c CustomTask) Execute() {
 	// implement your task execution in
 	fmt.Println("Task is Running: ", c.Information)
 }
 
 func main() {
-	// Init scheduler database
+	// Init goscheduler database
 	goscheduler.Initialize(&goscheduler.DatabaseConfig{
 		URI: "redis://127.0.0.1:6379/8",
 	})
 
-	// A task should be execute in 10 seconds
+	// A task should be executed in 10 seconds
 	task := &CustomTask{
 		ID:          "123",
 		Start:       time.Now().UTC(),
 		End:         time.Now().UTC().Add(time.Duration(10) * time.Second),
 		Information: "this is a task message message",
 	}
+
 	// first schedule the task at 10 seconds later
 	goscheduler.Schedule(task)
 	// however we decide to boot the task immediately
 	goscheduler.Boot(task)
 
+	// let's sleep 2 secs wait for the retult
 	time.Sleep(time.Second * 2)
 }
