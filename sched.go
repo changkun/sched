6e5c13e500aeac2f80285357cd89c0f89f62545a
Ou Changkun <hi@changkun.us>
2019-09-06 18:06:46 +0300

    sched: use reflect.Value.IsZero in Go 1.13 (#7)

--- sched.go
+++ sched.go
@@ -8,7 +8,6 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"reflect"
 	"strings"
 	"sync/atomic"
 	"time"
@@ -120,35 +119,6 @@ func (s *sched) recover(ts ...Task) (futures []TaskFuture, err error) {
 	}
 	s.resume()
 	return
-}
-
-func (s *sched) load(id string, t Task) (TaskFuture, error) {
-	r := &record{ID: id}
-	if err := r.read(); err != nil {
-		return nil, err
-	}
-
-	data, _ := json.Marshal(r.Data)
-
-	// Note: The following comparison provides a generic mechanism in golang, which
-	// unmarshals an unknown type of data into acorss multiple into an arbitrary variable.
-	//
-	// temp1 holds for a unset value of t, and temp2 tries to be set by json.Unmarshal.
-	//
-	// In the end, if temp1 and temp2 are appropriate type of tasks, then temp2 should
-	// not DeepEqual to temp1, because temp2 is setted by store data.
-	// Otherwise, the determined task is inappropriate type to be scheduled, jump to
-	// next record and see if it can be scheduled.
-	temp1 := reflect.New(reflect.ValueOf(t).Elem().Type()).Interface().(Task)
-	temp2 := reflect.New(reflect.ValueOf(t).Elem().Type()).Interface().(Task)
-	json.Unmarshal(data, &temp2)
-	if reflect.DeepEqual(temp1, temp2) || temp2 == nil || !temp2.IsValidID() {
-		return nil, nil
-	}
-	temp2.SetID(id)
-	temp2.SetExecution(r.Execution)
-	future, _ := s.tasks.push(temp2)
-	return future, nil
 }
 
 // submit given tasks
